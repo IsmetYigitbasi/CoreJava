eb5d467b6904ad657304c2c37d54b888ca1236ae
IsmetYigitbasi <ismetyigitbasi@gmail.com>
2020-10-07 23:32:05 +0200

    Added forLoop

---
+++ src/main/java/ForLoop.java
@@ -0,0 +1,16 @@
+public class ForLoop {
+
+    /*
+        When you know exactly how many times you want to loop, then use for loop instead of a while loop
+        Moreover it is more flexible than foreach loop.
+     */
+
+    public static void main(String[] args) {
+
+        for (int i = 0; i < 3; i++) {
+
+            System.out.println("This is a for loop");
+
+        }
+    }
+}
